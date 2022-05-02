963204b49a62b76c1f56a568086c380c8925f640
KangPeiLun <374774222@qq.com>
2022-05-02 15:45:48 +0800

    Update draw_data.py

--- draw_data.py
+++ draw_data.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from PIL import Image, ImageColor, ImageDraw
 import os
 
@@ -13,54 +14,57 @@ def colors_per_class(data='Indian'):
     :return:
     '''
     # colors 记录每种数据集 不同类别的颜色表，索引[0]是类别名，索引[1]是十六进制颜色值
-    if data == 'Indian':
+        if data == 'Indian':
         colors = {
-            1: ['CornNotill', '#53ab48'],
-            2: ['CornMintill', '#89ba43'],
-            3: ['Corn', '#42845b'],
-            4: ['GrassPature', '#3c8345'],
-            5: ['GrassTrees', '#905236'],
-            6: ['HayWindrow', '#69bcc8'],
-            7: ['SoybeanNotill', '#ffffff'],
-            8: ['SoybeanMintill', '#c7b0c9'],
-            9: ['SoybeanClean', '#da332c'],
-            10: ['Wheat', '#772324'],
-            11: ['Woods', '#57585a'],
-            12: ['BuilGraTrDri', '#e0db54'],
-            13: ['StoSteTower', '#d98e34'],
-            14: ['Alfalfa', '#54307e'],
-            15: ['GrassPastMow', '#e3775b'],
-            16: ['Oats', '#9d5796'],
+            # 0: ['others', '#000000'],
+            0: ['CornNotill', '#4fab49'],
+            1: ['CornMintill', '#88ba43'],
+            2: ['Corn', '#3e845c'],
+            3: ['GrassPature', '#388445'],
+            4: ['GrassTrees', '#915236'],
+            5: ['HayWindrow', '#67bcc8'],
+            6: ['SoybeanNotill', '#ffffff'],
+            7: ['SoybeanMintill', '#c7b0ca'],
+            8: ['SoybeanClean', '#db312d'],
+            9: ['Wheat', '#782424'],
+            10: ['Woods', '#2c6ab4'],
+            11: ['BuilGraTrDri', '#e0dd54'],
+            12: ['StoSteTower', '#d98e35'],
+            13: ['Alfalfa', '#54307d'],
+            14: ['GrassPastMow', '#e4775a'],
+            15: ['Oats', '#9d5796'],
         }
     elif data == 'Pavia':
         colors = {
-            1: ['Asphalt', '#c7c9cb'],
-            2: ['Meadows', '#6eb145'],
-            3: ['Gravel', '#69bcc8'],
-            4: ['Trees', '#3a7a42'],
-            5: ['Metal Sheets', '#9d5796'],
-            6: ['Bare Soil', '#945331'],
-            7: ['Bitumen', '#752d79'],
-            8: ['Bricks', '#da332c'],
-            9: ['Shadows', '#e0db54'],
+            # 0: ['others', '#000000'],
+            0: ['Asphalt', '#c9cacc'],
+            1: ['Meadows', '#6db146'],
+            2: ['Gravel', '#67bcc8'],
+            3: ['Trees', '#387b42'],
+            4: ['Metal Sheets', '#9d5796'],
+            5: ['Bare Soil', '#955333'],
+            6: ['Bitumen', '#742d78'],
+            7: ['Bricks', '#db312d'],
+            8: ['Shadows', '#e0dd54'],
         }
     elif data == 'Houston':
         colors = {
-            1: ['Healthy Grass', '#50ac48'],
-            2: ['Stressed Grass', '#89bb43'],
-            3: ['Synthetic Grass', '#3d855c'],
-            4: ['Trees', '#368545'],
-            5: ['Soil', '#915135'],
-            6: ['Water', '#65bdc8'],
-            7: ['Residential', '#ffffff'],
-            8: ['Commercial', '#c7b0cb'],
-            9: ['Road', '#db2f2b'],
-            10: ['Highway', '#792223'],
-            11: ['Railway', '#3265a7'],
-            12: ['Parkinig Lot1', '#e0dd53'],
-            13: ['Parkinig Lot2', '#db8e34'],
-            14: ['Tennis Court', '#542f7d'],
-            15: ['Running Track', '#e5775a'],
+            # 0: ['others', '#000000'],
+            0: ['Healthy Grass', '#4fac49'],
+            1: ['Stressed Grass', '#89bb43'],
+            2: ['Synthetic Grass', '#3d855c'],
+            3: ['Trees', '#388545'],
+            4: ['Soil', '#915136'],
+            5: ['Water', '#67bdc9'],
+            6: ['Residential', '#ffffff'],
+            7: ['Commercial', '#c8b1cb'],
+            8: ['Road', '#dc2f2d'],
+            9: ['Highway', '#792323'],
+            10: ['Railway', '#3565a7'],
+            11: ['Parkinig Lot1', '#e1dd54'],
+            12: ['Parkinig Lot2', '#db8e35'],
+            13: ['Tennis Court', '#55307d'],
+            14: ['Running Track', '#e5775a'],
         }
 
     return colors
@@ -79,6 +83,10 @@ def create_image(data='Indian'):
     return image
 
 
+def draw_one_pixel(draw, xy, fill):
+    draw.point(xy=xy, fill=fill)
+
+    
 def draw_data(positions, predicts, data='Indian'):
     '''
     绘制不同数据集的预测类别颜色图
@@ -92,14 +100,17 @@ def draw_data(positions, predicts, data='Indian'):
     '''
     ImageColor模块使用方法见以下连接
     https://www.jianshu.com/p/f63dc8b5445d?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
-    ImageColor.getrgb() 将十六进制颜色值转换为 RGB值
     '''
-    for pos, pre in zip(positions, predicts):
-        draw.point(pos, fill=ImageColor.getrgb(colors[pre][1]))     # 在对应索引位置处的像素上色
+    with ThreadPoolExecutor(500) as t:                                          # 使用多线程进行处理
+        for pos, pre in zip(positions, predicts):
+            # draw.point(list(pos), fill=ImageColor.getrgb(colors[pre][1]))     # 在对应索引位置处的像素上色
+            xy = (pos[1], pos[0])
+            fill = ImageColor.getrgb(colors[pre][1])
+            t.submit(draw_one_pixel, draw=draw, xy=xy, fill=fill)
 
     check_dir(f'predict')
     image.save(os.path.join(f'predict/{data}.png'))
-    image.show(title=f"{data}'s predict result")
+    # image.show(title=f"{data}'s predict result")
 
 
 if __name__ == '__main__':
